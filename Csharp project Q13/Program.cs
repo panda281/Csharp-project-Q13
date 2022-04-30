0ade4f68ea00b481ec4b6d6eb3ef95601893d008
Abinet Tamiru <Abinettamiru@outlook.com>
2022-04-30 23:17:33 +0300

    Add project files.

---
+++ Csharp project Q13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_project_Q13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please insert a number: ");
+            int n = int.Parse(Console.ReadLine());
+            bool prime = true;
+            
+                for(int i = 2; i<= Math.Sqrt(n); i++)
+                {
+                    if(n%i==0)
+                    {
+                        prime = false;
+                        
+                    }
+                   
+                }
+
+                if(prime)
+                {
+                    Console.WriteLine("its a prime number");
+                }
+                else
+                {
+                    Console.WriteLine("its not a prime number");
+            }
+            
+        }
+    }
+}
